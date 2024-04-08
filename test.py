f7426185ab6e2d580cdf09beb136e1538da2f43d
Kamran Khan Alwi <82154070+KAMRANKHANALWI@users.noreply.github.com>
2024-04-09 00:19:42 +0530

    browserless-scraping-initial-commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from PIL import Image
+from datetime import datetime
+
+current_datetime = datetime.now()
+current_datetime = current_datetime.strftime("%d_%m_%Y_%H_%M_%S")
+img_download_path = f'CaptchaImg/{current_datetime}.png'
+
+
+image_path = 'CaptchaImg/screenshot.png'
+# cropped_image = image.crop((left, top, right, bottom))
+crop_box = (1965, 685, 2835, 760)
+image = Image.open(image_path)
+cropped_image = image.crop(crop_box)
+
+# Save the cropped image to a file
+cropped_image.save(img_download_path)
